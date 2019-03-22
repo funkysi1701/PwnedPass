16bc2d0951604475ffc49f0207418bf451db7f80
Simon Foster <funkysi1701@gmail.com>
2019-03-22 21:38:54 +0000

    March (#54)
    * change build files
    
    * remove characters
    
    * change background of icons to black
    
    * install stylecop analysers (#39)
    
    * update nuget (#40)
    
    * Stylecop (#41)
    
    * add stylecop.json and other settings
    
    * run build on every commit
    
    * run whitesource/stylecop everytime
    
    * tidy up
    
    * nuget update (#42)
    
    * add viewmodel (#43)
    
    * add viewmodel
    
    * add more pages to use viewmodel (#46)
    
    * add more pages to use viewmodel
    
    * missed from last commit
    
    * add pastes to results (#47)
    
    * Paste2 (#48)
    
    * tweak wording of pastes
    
    * tidy
    
    * Vs2019 (#49)
    
    * change build to use vs2019
    
    * change build to use vs2019
    
    * add padding for UWP
    
    * i always forget something
    
    * add call to my new api (#50)
    
    * add new yml files for build win or android (#51)
    
    * reset a couple of views

---
+++ PwnedPasswords/PwnedPasswords/Model/Pastes.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using System;
+
+namespace PwnedPasswords
+{
+    public class Pastes
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime? Date { get; set; }
+        public int EmailCount { get; set; }
+    }
+}
