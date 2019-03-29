9211d7e9c3a075dbe7e186be0b218b480752c722
Simon Foster <funkysi1701@gmail.com>
2019-03-29 21:10:35 +0000

    Tests (#8)
    * add tests
    
    * add test step
    
    * add nuget.config
    
    * do i need this line?
    
    * not needed
    
    * wrong config in test runner
    
    * try to fix path to use with tests
    
    * add separate build
    
    * remove test step
    
    * change to tests
    
    * remove build that was failing anyway

---
+++ PwnedPass.Test/AppInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace PwnedPass.Test
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                string dir1 = Directory.GetCurrentDirectory();
+                string dir2 = Directory.GetParent(dir1).ToString();
+                string dir3 = Directory.GetParent(dir2).ToString();
+                string dir4 = Directory.GetParent(dir3).ToString();
+                string dir5 = Directory.GetParent(dir4).ToString();
+#if DEBUG
+                string mode = "Debug";
+#else
+                string mode = "Release";
+#endif
+                return ConfigureApp.Android.Debug().ApkFile(dir5 + "\\s\\PwnedPasswords\\PwnedPasswords.Android\\bin\\" + mode + "\\pwnedpasswords.pwnedpasswords.apk").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
