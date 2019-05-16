087e5bbe5c947365411fc595b55a5ac81391d5bb
Simon Foster <funkysi1701@gmail.com>
2019-05-16 11:49:27 +0100

    Beta (#69)

--- PwnedPasswords/PwnedPasswords/Cache.cs
+++ PwnedPasswords/PwnedPasswords/Cache.cs
@@ -42,7 +42,7 @@ namespace PwnedPasswords
                         data.Id = 1;
                         App.Database.SaveHIBP(data);
                         App.Database.EmptyDataBreach();
-                        string result = App.GetAPI.GetHIBP("https://pwnedpassapi.azurewebsites.net/api/HIBP/GetBreaches");
+                        string result = App.GetAPI.GetHIBP("https://pwnedpassapifsi.azurewebsites.net/api/HIBP/GetBreaches");
                         if (result != null && result.Length > 0)
                         {
                             JArray job = (JArray)JsonConvert.DeserializeObject(result);
@@ -118,7 +118,7 @@ namespace PwnedPasswords
         /// <returns>long</returns>
         public static long GetAccounts()
         {
-            string result = App.GetAPI.GetHIBP("https://pwnedpassapi.azurewebsites.net/api/HIBP/GetBreaches");
+            string result = App.GetAPI.GetHIBP("https://pwnedpassapifsi.azurewebsites.net/api/HIBP/GetBreaches");
             long count = 0;
             if (result != null && result.Length > 0)
             {
@@ -141,7 +141,7 @@ namespace PwnedPasswords
         /// <returns>int</returns>
         public static int GetBreach()
         {
-            string result = App.GetAPI.GetHIBP("https://pwnedpassapi.azurewebsites.net/api/HIBP/GetBreaches");
+            string result = App.GetAPI.GetHIBP("https://pwnedpassapifsi.azurewebsites.net/api/HIBP/GetBreaches");
             int count = 0;
             if (result != null && result.Length > 0)
             {
